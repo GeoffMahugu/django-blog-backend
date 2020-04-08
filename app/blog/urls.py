4d3ffd1d80fd14d28813ecf69a12faaa0264d917
geoff <mac@macs-MacBook-Pro.local>
2020-04-08 19:33:24 +0300

    Current status

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -12,8 +12,10 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 
 from rest_framework_simplejwt.views import (
@@ -25,4 +27,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # url(r'^api/v1/account/',
     #     include('wayapay.account.api.urls', namespace='api-accounts')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
